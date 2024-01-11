265772ceac68b44b31fefe34bbe811c840232876
Ayalni2289 <alininspor@hotmail.com>
2024-01-11 21:30:54 +0300

    CORS güvenlik etkinleştirme.

--- api/server.js
+++ api/server.js
@@ -9,6 +9,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 //router
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Tüm domainlere izin ver
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 const imageRoute = require("./routes/images.js");
 const patientRoute = require("./routes/patient.js");
