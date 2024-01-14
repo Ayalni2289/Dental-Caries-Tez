6526476e4796c6f603dc403f03dd6c2c2874c8a8
Ayalni2289 <alininspor@hotmail.com>
2024-01-14 21:42:44 +0300

    HomePage uptadet with button types and added prediction

--- client/src/components/patient/Buttons.jsx
+++ client/src/components/patient/Buttons.jsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 import Add from "./AddPatient";
-import { Button, Upload, message } from "antd";
+import { Button, Table, Upload, message, Tag, Typography  } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
+
 
 const Buttons = () => {
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [result, setResult] = useState(null);
   const [error, setError] = useState(null);
-  //const api_url = "https://cff3-188-132-140-102.ngrok-free.app"; Your API URL here
+  
+  const api_url = "https://464a-188-132-140-102.ngrok-free.app"; //Your API URL here
 
   const handleFileUpload = async (file) => {
     const formData = new FormData();
     formData.append('image', file);
 
     try {
-      const response = await fetch(process.env.Api_Url_Ai, {
+      const response = await fetch(api_url, {
         method: 'POST',
         body: formData,
       });
@@ -22,7 +24,8 @@ const Buttons = () => {
       if (response.ok) {
         const resultData = await response.json();
         setResult(resultData);
-        message.success(`${file.name} dosyası başarıyla yüklendi`);
+
+        message.success(`Dosya başarıyla yüklendi`);
       } else {
         const errorData = await response.json();
         setError(errorData.message);
@@ -37,7 +40,7 @@ const Buttons = () => {
     name: "file",
     action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
     headers: {
-      authorization: "authorization-text",
+      //authorization: "authorization-text",
     },
     onChange(info) {
       if (info.file.status !== "uploading") {
@@ -51,6 +54,32 @@ const Buttons = () => {
     },
   };
 
+  const columns = [
+    {
+      title: 'Field',
+      dataIndex: 'field',
+      key: 'field',
+    },
+    {
+      title: 'Value',
+      dataIndex: 'value',
+      key: 'value',
+      render: (text, record) => {
+        if (record.field === 'Predicted Class') {
+          return <Tag color="green">{text}</Tag>;
+        }
+        return text;
+      },
+    },
+  ];
+
+  const data = result
+  ? [
+      { key: '1', field: 'Predicted Class', value: result.predicted_class },
+      { key: '2', field: 'Confidence', value: result.confidence },
+    ]
+  : [];
+  
   return (
     <div className="products-wrapper flex flex-row gap-10 w-full items-center justify-center">
       <div
@@ -61,30 +90,23 @@ const Buttons = () => {
       >
         <p className="text-white font-mono text-sm md:text-xl">Hasta Ekle</p>
       </div>
-      <Upload {...props} accept=".png, .jpg, .jpeg" type="file" >
-        <div
-          className="product-item border hover:shadow-lg rounded-md
-       cursor-pointer transition-all select-none
-       bg-green-700 flex justify-center items-center hover:opacity-80 min-h-[180px] min-w-[180px]"
-          // YAPAY ZEKA ENTEGRASYONU !
-        >
-          <Button
+      <Upload {...props} accept=".png, .jpg, .jpeg" type="file" listType="picture" >
+        <div>
+          <Button 
             icon={<UploadOutlined />}
             className="text-white font-mono text-sm bg-fuchsia-700 rounded-2xl font-semibold"
           >
-            Teşhis İçin Fotoğraf Yükleyiniz
+                        Teşhis İçin Fotoğraf Yükleyiniz
+
           </Button>
         </div>
       </Upload>
-      <div>
-      {result && (
-              <div>
-                <p>Predicted Class: {result.predicted_class}</p>
-                <p>Confidence: {result.confidence}</p>
-              </div>
-            )}
-            {error && <p>{error}</p>}
-      </div>
+      <Table
+      dataSource={data}
+      columns={columns}
+      bordered
+      pagination={false}
+    />
       <Add
         isAddModalOpen={isAddModalOpen}
         setIsAddModalOpen={setIsAddModalOpen}
